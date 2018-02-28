e544d046e641eb896be34a388cdc6bc7e889b97b
serge <serge@a73c6d72-0a43-5a86-bf86-ce57b7db7bff>
2018-02-28 20:58:18 +0000

    start
    git-svn-id: https://github.com/ltv88/demoShop.git/trunk@1 a73c6d72-0a43-5a86-bf86-ce57b7db7bff
    
    
    git-svn-id: https://github.com/ltv88/demoShop.git/trunk@4 a73c6d72-0a43-5a86-bf86-ce57b7db7bff

--- src/main/java/Header.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by 88lit on 3/1/2018.
- */
-public class Header {
-}
---
+++ src/test/java/Login.java
@@ -0,0 +1,9 @@
+/**
+ * Created by 88lit on 2/28/2018.
+ */
+public class Login {
+
+    public void hello(){
+        System.out.println("");
+    }
+}
